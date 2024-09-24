0dbbb5c5d2a6844e02d8d58a04e8dfc56981551a
Mingsheng Gan <mingshengg@outlook.com>
2024-09-24 10:44:02 +0800

    Setup CRA

---
+++ web/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-link">
+          Upload Test
+        </p>
+      </header>
+      <body className="App-body">
+        Hello world
+      </body>
+    </div>
+  );
+}
+
+export default App;
